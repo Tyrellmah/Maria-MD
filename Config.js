b6bc22e9a02a9c9d90946d32ce57eda793ee749f
Tyrellmah <147133788+Tyrellmah@users.noreply.github.com>
2024-05-03 22:38:52 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,17 +3,17 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.ownernumber = [ process.env.OWNER_NUMBER || "254758299822"],
+global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘Tyrellmah✘𓆪✰",
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Tlmah-IV",//name of the bot
 
 //sticker details
 global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.packname = 'Sticker By'
+global.packname = 'Sticker By Tlmah'
 global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 //console view/theme
 global.themeemoji = '🧩'
